dfb9b6922b36fed49ae94a34f7814000c65bc36d
SunnySWY <apple0105@126.com>
2017-06-05 14:10:45 +0800

    异步子进程读文件，虽然比较low，但好歹也是跨平台的

--- lib/asynchelper.py
+++ lib/asynchelper.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 import asyncio, selectors, sys
 
+@asyncio.coroutines
+def async_read(file_name):
+    this_future = asyncio.Future(loop=EventLoop)
+    read_code = '''with open({0}) as f:print(f.readlines())'''.format(file_name)
+    fork_pro = EventLoop.subprocess_exec(lambda: DateProtocol(this_future),
+                                         sys.executable, '-c', read_code)
+    transport, protocol = yield from fork_pro
+    yield from this_future
+    transport.close()
+    data = bytes(protocol.output)
+    return data.decode('ascii').rstrip()
+
+class DateProtocol(asyncio.SubprocessProtocol):
+    '''子进程间的交互协议'''
+    def __init__(self, exit_future):
+        self.exit_future = exit_future
+        self.output = bytearray()
+
+    def pipe_data_received(self, fd, data):
+        self.output.extend(data)
+
+    def process_exited(self):
+        self.exit_future.set_result(True)
+
 class CurrentEventLoop(object):
     '''根据不同的操作系统
     初始化IO轮询方式以及事件循环'''
